ce2240408b26ff05ab30036da094924ced17029b
Konstantin Kolodiy <bonewell@yandex.ru>
2018-11-04 22:18:08 +0200

    Initial Commit

---
+++ CityBoom/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CityBoom
+//
+//  Created by Константин Колодий on 04/11/2018.
+//  Copyright © 2018 Константин Колодий. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
